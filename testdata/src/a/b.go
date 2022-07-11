0abbd0bf79b1aea1d44f350e119e50542316ec6c
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2022-07-11 07:40:31 +0300

    Remove all the dependencies in ginkgo & gomega
    By setting the Analyzer's RunDespiteErrors field to `true`, the type
    check is skiped and the linter can work without the need to actually
    import ginkgo and gomega.
    
    This will ease the integration with other applications, such as
    golangci-lint.

--- testdata/src/a/b.go
+++ testdata/src/a/b.go
@@ -1,10 +1,5 @@
 package a
 
-import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-)
-
 var _ = Describe("Supress wrong length check", func() {
 	Context("test ginkgo-linter:ignore-len-assert-warning", func() {
 		It("should ignore length warning", func() {
@@ -13,7 +8,6 @@ var _ = Describe("Supress wrong length check", func() {
 			Expect(len("abc")).Should(Equal(3)) // want `ginkgo-linter: wrong length assertion; consider using .Expect\("abc"\)\.Should\(HaveLen\(3\)\). instead`
 			Expect("123").To(HaveLen(3))
 			/*
-
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
 				aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
