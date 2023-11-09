289fb3c4c01fd3087421835fee535e89ae6284cb
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2023-11-09 11:47:22 +0200

    New rule(s): Wrong Usage of the `MatchError` gomega Matcher
    The `MatchError` gomega matcher asserts an error value (and if it's not nil).
    
    There are four valid formats for using this Matcher:
    * error value; e.g. `Expect(err).To(MatchError(anotherErr))`
    * string, to be equal to the output of the `Error()` method; e.g. `Expect(err).To(MatchError("Not Found"))`
    * A gomega matcher that asserts strings; e.g. `Expect(err).To(MatchError(ContainSubstring("Found")))`
    * [from v0.29.0] a function that receive a single error parameter and returns a single boolean value.
      In this format, an additional single string parameter, with the function description, is also required; e.g.
      `Expect(err).To(MatchError(isNotFound, "is the error is a not found error"))`
    
    These four format are checked on runtime, but sometimes it's too late. ginkgolinter performs a static analysis and so it
    will find these issues on build time.
    
    ginkgolinter checks the following:
    * Is the first parameter is one of the four options above.
    * That there are no additional parameters passed to the matcher; e.g.
      `MatchError(isNotFoundFunc, "a valid description" , "not used string")`. In this case, the matcher won't fail on run
      time, but the additional parameters are not in use and ignored.
    * If the first parameter is a function with the format of `func(error)bool`, ginkgolinter makes sure that the second
      parameter exists and its type is string.

---
+++ testdata/src/a/matcherror/matcherror.go
@@ -0,0 +1,161 @@
+package matcherror
+
+import (
+	"errors"
+	"fmt"
+	"github.com/onsi/gomega/types"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+type e string
+
+func (err e) Error() string {
+	return string(err)
+}
+
+func isExample(err error) bool {
+	return err.Error() == "example"
+}
+
+func compareString(err error, s string) bool {
+	return err.Error() == s
+}
+
+func f1() error {
+	return errors.New("example")
+}
+
+func f2() string {
+	return "example"
+}
+
+func f3() (string, error) {
+	return "example", errors.New("example")
+}
+
+type customMatcher struct{}
+
+func (customMatcher) Match(actual interface{}) (success bool, err error) {
+	return true, nil
+}
+func (customMatcher) FailureMessage(actual interface{}) (message string) {
+	return "FailureMessage"
+}
+func (customMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	return "NegatedFailureMessage"
+}
+
+func CustomMatcher() customMatcher {
+	return customMatcher{}
+}
+
+func CustomMatcherP() *customMatcher {
+	return &customMatcher{}
+}
+
+var _ = Describe("Check MatchError", func() {
+	e1 := e("example")
+	e2 := e("example")
+
+	It("one argument - valid", func() {
+		Expect(e1).To(MatchError(e2))
+		Expect(errors.New("example")).To(MatchError("example"))
+		Expect(e1).To(MatchError("example"))
+		Expect(e1).To(MatchError(ContainSubstring("amp")))
+		Expect(e1).To(MatchError(Not(ContainSubstring("abc"))))
+		Expect(e1).To(MatchError(CustomMatcher()))
+		Expect(e1).To(MatchError(CustomMatcherP()))
+		Expect(e1).To(MatchError(customMatcher{}))
+		Expect(e1).To(MatchError(&customMatcher{}))
+
+		var gm types.GomegaMatcher = customMatcher{}
+		Expect(e1).To(MatchError(gm))
+	})
+
+	It("one argument - error", func() {
+		Expect("example").To(MatchError("example")) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \("example"\)`
+		one := 1
+		Expect(one).To(MatchError("example")) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(one\)`
+		Expect(e1).To(MatchError(isExample))  // want `ginkgo-linter: missing function description as second parameter of MatchError`
+		Expect(e1).To(MatchError(f1))         // want `ginkgo-linter: MatchError first parameter \(f1\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+	})
+
+	It("two arguments - valid", func() {
+		Expect(e1).To(MatchError(func(_ error) bool {
+			return true
+		}, "a description"))
+		Expect(e1).ToNot(MatchError(func(_ error) bool {
+			return false
+		}, "a description"))
+		Expect(e1).To(MatchError(func(err error) bool {
+			err.Error()
+			return true
+		}, "a description"))
+		Expect(e1).ToNot(MatchError(func(err error) bool {
+			err.Error()
+			return false
+		}, "a description"))
+		Expect(e1).To(MatchError(isExample, "is it the example error"))
+		Expect(e1).To(MatchError(isExample, fmt.Sprint("is it the example error")))
+	})
+
+	It("two arguments - wrong usage", func() {
+		Expect(e1).To(MatchError(e2, "not used"), "this text is ok")                      // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+		Expect(e1).To(MatchError("example", "not used"), "this text is ok")               // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("example"\). instead`
+		Expect(e1).To(MatchError(ContainSubstring("amp"), "not used"), "this text is ok") // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(ContainSubstring\("amp"\)\). instead`
+	})
+
+	It("two arguments - invalid", func() {
+		Expect(e1).To(MatchError(1, "not used"), "wrong type - int")         // want `ginkgo-linter: MatchError first parameter \(1\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+		Expect(e1).To(MatchError(compareString, "wrong function signature")) // want `ginkgo-linter: MatchError first parameter \(compareString\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+	})
+
+	It("two arguments. Second is not a string", func() {
+		Expect(e1).To(MatchError(isExample, 1))                         // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+		Expect(e1).To(MatchError(isExample, func() int { return 1 }))   // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+		Expect(e1).To(MatchError(isExample, func() int { return 1 }())) // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+	})
+
+	It("multiple arguments", func() {
+		Expect(e1).To(MatchError(isExample, "is it the example error", "not used"))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(isExample, "is it the example error"\). instead`
+		Expect(e1).To(MatchError(isExample, "is it the example error", "not used", "not used")) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(isExample, "is it the example error"\). instead`
+		Expect(e1).To(MatchError(e2, "not used", "not used"))                                   // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+		Expect(e1).To(MatchError("example", "not used", "not used"))                            // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("example"\). instead`
+		Expect(e1).To(MatchError(ContainSubstring("amp"), "not used", "not used"))              // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(ContainSubstring\("amp"\)\). instead`
+	})
+
+	It("funcs - valid", func() {
+		Expect(f1()).To(MatchError(errors.New("example")))
+		Expect(f1()).To(MatchError("example"))
+		Expect(f1()).To(MatchError(ContainSubstring("amp")))
+	})
+
+	It("funcs - invalid", func() {
+		Expect(f2()).To(MatchError(errors.New("example")))   // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		Expect(f2()).To(MatchError("example"))               // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		Expect(f2()).To(MatchError(ContainSubstring("amp"))) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		Expect(f3()).To(MatchError(errors.New("example")))   // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+		Expect(f3()).To(MatchError("example"))               // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+		Expect(f3()).To(MatchError(ContainSubstring("amp"))) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+	})
+
+	It("and - invalid", func() {
+		var err error = e1
+		Expect(err).To(And(Not(BeNil()), MatchError("expect", "not used")))               // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead`
+		Expect(err).To(And(MatchError("expect"), MatchError(e2)))                         // valid
+		Expect(err).To(And(MatchError("expect", "not used"), MatchError(e2, "not used"))) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead` `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+		Expect(err).To(And(MatchError("expect", "not used"), MatchError(e2)))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead`
+		Expect(err).To(And(MatchError("expect"), MatchError(e2, "not used")))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+	})
+
+	It("or - invalid", func() {
+		var err error = e1
+		Expect(err).To(Or(Not(BeNil()), MatchError("expect", "not used")))               // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead`
+		Expect(err).To(Or(MatchError("expect"), MatchError(e2)))                         // valid
+		Expect(err).To(Or(MatchError("expect", "not used"), MatchError(e2, "not used"))) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead` `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+		Expect(err).To(Or(MatchError("expect", "not used"), MatchError(e2)))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\("expect"\). instead`
+		Expect(err).To(Or(MatchError("expect"), MatchError(e2, "not used")))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .MatchError\(e2\). instead`
+	})
+})
---
+++ testdata/src/a/matcherror/matcherror.gomega.go
@@ -0,0 +1,116 @@
+package matcherror
+
+import (
+	"errors"
+	"fmt"
+	"github.com/onsi/gomega/types"
+
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+var _ = Describe("Check MatchError", func() {
+	e1 := e("example")
+	e2 := e("example")
+
+	It("one argument - valid", func() {
+		gomega.Expect(e1).To(gomega.MatchError(e2))
+		gomega.Expect(errors.New("example")).To(gomega.MatchError("example"))
+		gomega.Expect(e1).To(gomega.MatchError("example"))
+		gomega.Expect(e1).To(gomega.MatchError(gomega.ContainSubstring("amp")))
+		gomega.Expect(e1).To(gomega.MatchError(gomega.Not(gomega.ContainSubstring("abc"))))
+		gomega.Expect(e1).To(gomega.MatchError(CustomMatcher()))
+		gomega.Expect(e1).To(gomega.MatchError(CustomMatcherP()))
+		gomega.Expect(e1).To(gomega.MatchError(customMatcher{}))
+		gomega.Expect(e1).To(gomega.MatchError(&customMatcher{}))
+
+		var gm types.GomegaMatcher = customMatcher{}
+		gomega.Expect(e1).To(gomega.MatchError(gm))
+	})
+
+	It("one argument - error", func() {
+		gomega.Expect("example").To(gomega.MatchError("example")) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \("example"\)`
+		one := 1
+		gomega.Expect(one).To(gomega.MatchError("example")) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(one\)`
+		gomega.Expect(e1).To(gomega.MatchError(isExample))  // want `ginkgo-linter: missing function description as second parameter of MatchError`
+		gomega.Expect(e1).To(gomega.MatchError(f1))         // want `ginkgo-linter: MatchError first parameter \(f1\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+	})
+
+	It("two arguments - valid", func() {
+		gomega.Expect(e1).To(gomega.MatchError(func(_ error) bool {
+			return true
+		}, "a description"))
+		gomega.Expect(e1).ToNot(gomega.MatchError(func(_ error) bool {
+			return false
+		}, "a description"))
+		gomega.Expect(e1).To(gomega.MatchError(func(err error) bool {
+			err.Error()
+			return true
+		}, "a description"))
+		gomega.Expect(e1).ToNot(gomega.MatchError(func(err error) bool {
+			err.Error()
+			return false
+		}, "a description"))
+		gomega.Expect(e1).To(gomega.MatchError(isExample, "is it the example error"))
+		gomega.Expect(e1).To(gomega.MatchError(isExample, fmt.Sprint("is it the example error")))
+	})
+
+	It("two arguments - wrong usage", func() {
+		gomega.Expect(e1).To(gomega.MatchError(e2, "not used"), "this text is ok")                             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+		gomega.Expect(e1).To(gomega.MatchError("example", "not used"), "this text is ok")                      // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("example"\). instead`
+		gomega.Expect(e1).To(gomega.MatchError(gomega.ContainSubstring("amp"), "not used"), "this text is ok") // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(gomega\.ContainSubstring\("amp"\)\). instead`
+	})
+
+	It("two arguments - invalid", func() {
+		gomega.Expect(e1).To(gomega.MatchError(1, "not used"), "wrong type - int")         // want `ginkgo-linter: MatchError first parameter \(1\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+		gomega.Expect(e1).To(gomega.MatchError(compareString, "wrong function signature")) // want `ginkgo-linter: MatchError first parameter \(compareString\) must be error, string, GomegaMatcher or func\(error\)bool are allowed`
+	})
+
+	It("two arguments. Second is not a string", func() {
+		gomega.Expect(e1).To(gomega.MatchError(isExample, 1))                         // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+		gomega.Expect(e1).To(gomega.MatchError(isExample, func() int { return 1 }))   // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+		gomega.Expect(e1).To(gomega.MatchError(isExample, func() int { return 1 }())) // want `ginkgo-linter: The second parameter of MatchError must be the function description \(string\)`
+	})
+
+	It("multiple arguments", func() {
+		gomega.Expect(e1).To(gomega.MatchError(isExample, "is it the example error", "not used"))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(isExample, "is it the example error"\). instead`
+		gomega.Expect(e1).To(gomega.MatchError(isExample, "is it the example error", "not used", "not used")) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(isExample, "is it the example error"\). instead`
+		gomega.Expect(e1).To(gomega.MatchError(e2, "not used", "not used"))                                   // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+		gomega.Expect(e1).To(gomega.MatchError("example", "not used", "not used"))                            // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("example"\). instead`
+		gomega.Expect(e1).To(gomega.MatchError(gomega.ContainSubstring("amp"), "not used", "not used"))       // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(gomega\.ContainSubstring\("amp"\)\). instead`
+	})
+
+	It("funcs - valid", func() {
+		gomega.Expect(f1()).To(gomega.MatchError(errors.New("example")))
+		gomega.Expect(f1()).To(gomega.MatchError("example"))
+		gomega.Expect(f1()).To(gomega.MatchError(gomega.ContainSubstring("amp")))
+	})
+
+	It("funcs - invalid", func() {
+		gomega.Expect(f2()).To(gomega.MatchError(errors.New("example")))          // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		gomega.Expect(f2()).To(gomega.MatchError("example"))                      // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		gomega.Expect(f2()).To(gomega.MatchError(gomega.ContainSubstring("amp"))) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f2\(\)\)`
+		gomega.Expect(f3()).To(gomega.MatchError(errors.New("example")))          // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+		gomega.Expect(f3()).To(gomega.MatchError("example"))                      // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+		gomega.Expect(f3()).To(gomega.MatchError(gomega.ContainSubstring("amp"))) // want `ginkgo-linter: the MatchError matcher used to assert a non error type \(f3\(\)\)`
+	})
+
+	It("and - invalid", func() {
+		var err error = e1
+		gomega.Expect(err).To(gomega.And(gomega.Not(gomega.BeNil()), gomega.MatchError("expect", "not used")))        // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead`
+		gomega.Expect(err).To(gomega.And(gomega.MatchError("expect"), gomega.MatchError(e2)))                         // valid
+		gomega.Expect(err).To(gomega.And(gomega.MatchError("expect", "not used"), gomega.MatchError(e2, "not used"))) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead` `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+		gomega.Expect(err).To(gomega.And(gomega.MatchError("expect", "not used"), gomega.MatchError(e2)))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead`
+		gomega.Expect(err).To(gomega.And(gomega.MatchError("expect"), gomega.MatchError(e2, "not used")))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+	})
+
+	It("or - invalid", func() {
+		var err error = e1
+		gomega.Expect(err).To(gomega.Or(gomega.Not(gomega.BeNil()), gomega.MatchError("expect", "not used")))        // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead`
+		gomega.Expect(err).To(gomega.Or(gomega.MatchError("expect"), gomega.MatchError(e2)))                         // valid
+		gomega.Expect(err).To(gomega.Or(gomega.MatchError("expect", "not used"), gomega.MatchError(e2, "not used"))) // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead` `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+		gomega.Expect(err).To(gomega.Or(gomega.MatchError("expect", "not used"), gomega.MatchError(e2)))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\("expect"\). instead`
+		gomega.Expect(err).To(gomega.Or(gomega.MatchError("expect"), gomega.MatchError(e2, "not used")))             // want `ginkgo-linter: redundant MatchError arguments; consider removing them; consider using .gomega\.MatchError\(e2\). instead`
+	})
+
+})
