de1bcdb6a73082fb85b936188d77d8b95005bd0c
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2025-07-01 12:46:06 +0300

    Change how test-cli is running
    The cli-test is in the tests directory. It is a separate go module,
    because we don't want it to run with the unit tests when running
    
    go test ./...
    
    But this approach causes issues with automatic update of dependencies,
    because the go.mod in the tests directory does not get automatic
    updates, and then fauls in CI.
    
    Instead, this commit changes the cli-test to only run if the RUN_CLI_TESTS
    environment variable is set to "true".
    
    In addition, this commit stopped using the deprecated testscript.RunMain
    function, and replaces it with the recommended testscript.Main function.

--- tests/cli_test.go
+++ tests/cli_test.go
@@ -21,12 +21,16 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 
-	os.Exit(testscript.RunMain(m, map[string]func() int{
+	testscript.Main(m, map[string]func(){
 		"ginkgolinter": cli.Main,
-	}))
+	})
 }
 
 func TestCli(t *testing.T) {
+	if os.Getenv("RUN_CLI_TESTS") != "true" {
+		t.Skipf(`Skipping CLI tests. These tests are not unit tests, but more like integration tests.
+The tests will run when the RUN_CLI_TESTS environment variable is set to "true"`)
+	}
 	pwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
