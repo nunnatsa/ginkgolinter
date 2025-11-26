bd617ba10f0ddfd53bdd5d9d6daa5ffc5be63e2f
Patrick Ohly <patrick.ohly@intel.com>
2025-11-26 12:20:14 +0100

    support for wrappers
    Previously, linting was only done for function calls that the linter could
    identify by name as being one of the methods in Gomega that are known to return
    an Assertion or AsyncAssertion (Expect, Consistently, Eventually, etc.).
    
    Now wrappers are also supported. The basic idea is that any function call which
    returns any type that implements the Gomega assertion methods (To/Should, etc.)
    can be treated as the root of a Gomega expression call chain.
    
    Wrappers which take an offset as first parameter are not supported because
    the linter isn't smart enough to detect that the first parameter is an offset.
    Such wrappers should not be needed anymore thanks to ginkgo.Helper.
    
    The "missing assertion" error then needs to be a bit more vague because it
    cannot suggest what the appropriate assertion should be.

--- internal/gomegahandler/handler_test.go
+++ internal/gomegahandler/handler_test.go
@@ -2,327 +2,249 @@
 package gomegahandler
 
 import (
+	"errors"
+	"fmt"
 	"go/ast"
-	gotypes "go/types"
+	"os"
+	"path"
+
+	// gotypes "go/types"
 	"testing"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestGetGomegaHandler_dot(t *testing.T) {
-	name := ast.NewIdent("test.go")
-	file := &ast.File{
-		Name: name,
-		Imports: []*ast.ImportSpec{
-			{
-				Name: ast.NewIdent("."),
-				Path: &ast.BasicLit{Value: `"github.com/onsi/gomega"`},
-			},
+// run sets up a directory with the given code in main.go such that
+// importing gomega/ginkgo works, then invokes the callback for that file.
+//
+// The callback then can run checks and report failures through errors.
+//
+// This approach is necessary because stubbing a full analysis.Pass
+// instance is hard.
+func run(t *testing.T, code string, cb func(file *ast.File, pass *analysis.Pass) error) {
+	tmpDir := t.TempDir()
+	srcDir := path.Join(tmpDir, "src")
+	if err := os.Mkdir(srcDir, 0700); err != nil {
+		t.Fatalf("creating src dir failed: %v", err)
+	}
+
+	if err := os.CopyFS(srcDir, os.DirFS("../../testdata/src/a/vendor")); err != nil {
+		t.Fatalf("populating temp dir with vendored source failed: %v", err)
+	}
+
+	testFileName := path.Join(tmpDir, "main.go")
+	if err := os.WriteFile(testFileName, []byte(code), 0600); err != nil {
+		t.Fatalf("populating temp dir with test file code: %v", err)
+	}
+
+	analyzer := &analysis.Analyzer{
+		Name: "ginkgolinter",
+		Doc:  "test linter",
+		Run: func(pass *analysis.Pass) (any, error) {
+			for _, file := range pass.Files {
+				if file.Name.Name == "main" {
+					return nil, cb(file, pass)
+				}
+			}
+			return nil, errors.New("main file not found")
 		},
 	}
-
-	h := GetGomegaHandler(file, nil)
-	if h == nil {
-		t.Fatalf("should return dotHandler")
-	}
-	_, ok := h.(*dotHandler)
-	if !ok {
-		t.Error("should return dotHandler")
-	}
+	analysistest.Run(t, tmpDir, analyzer, ".")
 }
 
-func TestGetGomegaHandler_noname(t *testing.T) {
-	name := ast.NewIdent("test.go")
-	file := &ast.File{
-		Name: name,
-		Imports: []*ast.ImportSpec{
-			{
-				Path: &ast.BasicLit{Value: `"github.com/onsi/gomega"`},
-			},
-		},
-	}
-
-	h := GetGomegaHandler(file, nil)
-	if h == nil {
-		t.Fatalf("should return nameHandler")
-	}
-	n, ok := h.(*nameHandler)
-	if !ok {
-		t.Error("should return nameHandler")
-	}
-
-	if n.name != "gomega" {
-		t.Errorf("import name should be `gomega`, but it's %s", n.name)
-	}
-}
-
-func TestGetGomegaHandler_name(t *testing.T) {
-	name := ast.NewIdent("test.go")
-	file := &ast.File{
-		Name: name,
-		Imports: []*ast.ImportSpec{
-			{
-				Name: ast.NewIdent("name"),
-				Path: &ast.BasicLit{Value: `"github.com/onsi/gomega"`},
-			},
-		},
-	}
-
-	h := GetGomegaHandler(file, nil)
-	if h == nil {
-		t.Fatalf("should return nameHandler")
-	}
-	n, ok := h.(*nameHandler)
-	if !ok {
-		t.Error("should return nameHandler")
-	}
-
-	if n.name != "name" {
-		t.Errorf("import name should be `name`, but it's %s", n.name)
-	}
-}
-
-func TestGetGomegaHandler_no_gomega(t *testing.T) {
-	name := ast.NewIdent("test.go")
-	file := &ast.File{
-		Name: name,
-		Imports: []*ast.ImportSpec{
-			{
-				Name: ast.NewIdent("."),
-				Path: &ast.BasicLit{Value: `"github.com/onsi/ginkgo/v2"`},
-			},
-		},
-	}
-
-	h := GetGomegaHandler(file, nil)
-	if h != nil {
-		t.Fatalf("should return nil")
-	}
-}
-
-const actualName = "actual"
-
-var (
-	gVarVar     = ast.NewIdent("g")
-	gVarPointer = ast.NewIdent("g")
-	noGomegaVar = ast.NewIdent("g")
+func TestGetGomegaHandler(t *testing.T) {
+	for name, tt := range map[string]struct {
+		code         string
+		expectGomega bool
+		// expectInfo contains the expected info for each call expression,
+		// in depth-first preorder.
+		expectInfos []*GomegaBasicInfo
+	}{
+		"dot": {
+			code: `package main
+import (
+	. "github.com/onsi/gomega"
+	gtypes "github.com/onsi/gomega/types"
 )
 
-func newGomegaPass() *analysis.Pass {
-	return &analysis.Pass{
-		TypesInfo: &gotypes.Info{
-			Types: map[ast.Expr]gotypes.TypeAndValue{
-				gVarVar: {
-					Type: gotypes.NewNamed(gotypes.NewTypeName(0, gotypes.NewPackage(`github.com/onsi/gomega/internal`, ""), `Gomega`, &gotypes.Named{}), nil, nil),
-				},
-				gVarPointer: {
-					Type: gotypes.NewPointer(gotypes.NewNamed(gotypes.NewTypeName(0, gotypes.NewPackage(`github.com/onsi/gomega/types`, ""), `Gomega`, &gotypes.Named{}), nil, nil)),
-				},
-				noGomegaVar: {
-					Type: gotypes.NewPointer(gotypes.NewNamed(gotypes.NewTypeName(0, gotypes.NewPackage(`github.com/something/else`, ""), `somethingElse`, &gotypes.Named{}), nil, nil)),
-				},
-			},
-		},
-	}
+func other() interface{foo ()} {
+	return nil
 }
 
-func TestGomegaDotHandler_GetGomegaBasicInfo(t *testing.T) {
-	h := &dotHandler{
-		pass: newGomegaPass(),
-	}
-
-	for _, tc := range []struct {
-		name              string
-		exp               *ast.CallExpr
-		expectedOK        bool
-		expectedName      string
-		expectedGomegaVar bool
-	}{
-		{
-			name: "simple happy case",
-			exp: &ast.CallExpr{
-				Fun: ast.NewIdent(actualName),
-			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: false,
-		},
-		{
-			name: "non-ident func",
-			exp: &ast.CallExpr{
-				Fun: &ast.CallExpr{},
-			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
-		},
-		{
-			name: "var happy case gomega var",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   gVarVar,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: true,
-		},
-		{
-			name: "var happy case gomega pointer",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   gVarPointer,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: true,
-		},
-		{
-			name: "non-gomega var",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   noGomegaVar,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			info, ok := h.GetGomegaBasicInfo(tc.exp)
-			if ok != tc.expectedOK {
-				t.Errorf(`expected ok = "%t" but it's "%t"'`, tc.expectedOK, ok)
-			}
-
-			if !ok {
-				return
-			}
-
-			if info.MethodName != tc.expectedName {
-				t.Errorf(`expected name = "%s" but it's "%s"'`, tc.expectedName, info.MethodName)
-			}
-
-			if info.UseGomegaVar != tc.expectedGomegaVar {
-				t.Errorf(`expected gomega var to be %T, but it's %T'`, tc.expectedGomegaVar, info.UseGomegaVar)
-			}
-		})
-	}
+func myExpect(actual any) Assertion {
+	return Expect(actual)
 }
 
-func TestGomegaNameHandler_GetGomegaBasicInfo(t *testing.T) {
-	h := nameHandler{name: "gomega", pass: newGomegaPass()}
+func myEventually(actual any) AsyncAssertion {
+	return Eventually(actual)
+}
 
-	for _, tc := range []struct {
-		name              string
-		exp               *ast.CallExpr
-		expectedOK        bool
-		expectedName      string
-		expectedGomegaVar bool
-	}{
-		{
-			name: "happy usecase",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   ast.NewIdent("gomega"),
-					Sel: ast.NewIdent(actualName),
-				},
+func myEqual(expect any) gtypes.GomegaMatcher {
+	return Equal(expect)
+}
+
+func main() {
+	Expect(1)
+	Expect(1).Error()
+	NewWithT(nil).Expect(1)
+	Ω(1)
+	Eventually(0)
+	Consistently(0)
+	Consistently(0).WithTimeout(0).WithPolling(0)
+
+	other()
+	other().foo()
+
+	myExpect(1)
+	myEventually(1)
+
+	Equal(0)
+	myEqual(0)
+}
+`,
+			expectGomega: true,
+			expectInfos: []*GomegaBasicInfo{
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall, HasErrorMethod: true},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Ω", RootCallType: SyncAssertionCall},
+				{MethodName: "Eventually", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+
+				nil,
+				nil,
+
+				{MethodName: "myExpect", RootCallType: SyncAssertionCall},
+				{MethodName: "myEventually", RootCallType: AsyncAssertionCall},
+
+				{MethodName: "Equal", RootCallType: MatcherCall},
+				{MethodName: "myEqual", RootCallType: MatcherCall},
 			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: false,
 		},
-		{
-			name: "not a selector",
-			exp: &ast.CallExpr{
-				Fun: ast.NewIdent("name"),
+
+		"noname": {
+			code: `package main
+import (
+	"github.com/onsi/gomega"
+)
+
+func main() {
+	gomega.Expect(1)
+	gomega.Expect(1).Error()
+	gomega.NewWithT(nil).Expect(1)
+	gomega.Ω(1)
+	gomega.Eventually(0)
+	gomega.Consistently(0)
+	gomega.Consistently(0).WithTimeout(0).WithPolling(0)
+	gomega.Equal(0)
+}
+`,
+			expectGomega: true,
+			expectInfos: []*GomegaBasicInfo{
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall, HasErrorMethod: true},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Ω", RootCallType: SyncAssertionCall},
+				{MethodName: "Eventually", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+				{MethodName: "Equal", RootCallType: MatcherCall},
 			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
 		},
-		{
-			name: "CallExpr",
-			exp: &ast.CallExpr{
-				Fun: ast.NewIdent("name"),
+
+		"name": {
+			code: `package main
+import (
+	g "github.com/onsi/gomega"
+)
+
+func main() {
+	g.Expect(1)
+	g.Expect(1).Error()
+	g.NewWithT(nil).Expect(1)
+	g.Ω(1)
+	g.Eventually(0)
+	g.Consistently(0)
+	g.Consistently(0).WithTimeout(0).WithPolling(0)
+	g.Equal(0)
+}
+`,
+			expectGomega: true,
+			expectInfos: []*GomegaBasicInfo{
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall, HasErrorMethod: true},
+				{MethodName: "Expect", RootCallType: SyncAssertionCall},
+				{MethodName: "Ω", RootCallType: SyncAssertionCall},
+				{MethodName: "Eventually", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+				{MethodName: "Consistently", RootCallType: AsyncAssertionCall},
+				{MethodName: "Equal", RootCallType: MatcherCall},
 			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
 		},
-		{
-			name: "no gomega",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   ast.NewIdent("notgomega"),
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
-		},
-		{
-			name: "gomega variable",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   gVarVar,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: true,
-		},
-		{
-			name: "gomega pointer",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   gVarPointer,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        true,
-			expectedName:      actualName,
-			expectedGomegaVar: true,
-		},
-		{
-			name: "gomega variable from non gomega function",
-			exp: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X:   noGomegaVar,
-					Sel: ast.NewIdent(actualName),
-				},
-			},
-			expectedOK:        false,
-			expectedName:      "",
-			expectedGomegaVar: false,
+
+		"none": {
+			code: `package main
+`,
+			expectGomega: false,
 		},
 	} {
-		t.Run(tc.name, func(t *testing.T) {
-			info, ok := h.GetGomegaBasicInfo(tc.exp)
-			if ok != tc.expectedOK {
-				t.Errorf(`expected ok = "%t" but it's "%t"'`, tc.expectedOK, ok)
-			}
-			if !ok {
-				return
-			}
-			if info.MethodName != tc.expectedName {
-				t.Errorf(`expected name = "%s" but it's "%s"'`, tc.expectedName, info.MethodName)
-			}
-			if info.UseGomegaVar != tc.expectedGomegaVar {
-				t.Errorf(`expected gomega var to be %T, but it's %T'`, tc.expectedGomegaVar, info.UseGomegaVar)
-			}
+		t.Run(name, func(t *testing.T) {
+			run(t, tt.code, func(file *ast.File, pass *analysis.Pass) error {
+				h := GetGomegaHandler(file, pass)
+				if (h != nil) != tt.expectGomega {
+					return fmt.Errorf("expectNoGomega %v, got handler %#v", tt.expectGomega, h)
+				}
+				if h == nil {
+					return nil
+				}
+
+				infoIndex := 0
+				for n := range ast.Preorder(file) {
+					stmt, ok := n.(*ast.ExprStmt)
+					if !ok {
+						continue
+					}
+					call, ok := stmt.X.(*ast.CallExpr)
+					if !ok {
+						continue
+					}
+
+					if infoIndex >= len(tt.expectInfos) {
+						return fmt.Errorf("unexpected call statement %#v", call)
+					}
+					actualInfo := h.GetGomegaBasicInfo(call)
+					expectInfo := tt.expectInfos[infoIndex]
+					currentIndex := infoIndex
+					infoIndex++
+					if (actualInfo == nil) != (expectInfo == nil) {
+						return fmt.Errorf("#%d: expected %#v, actual %#v", currentIndex, expectInfo, actualInfo)
+					}
+					if actualInfo == nil {
+						continue
+					}
+
+					// Cannot compare RootCall, but it has to be set.
+					if actualInfo.RootCall == nil {
+						return fmt.Errorf("#%d: missing RootCall in %#v", currentIndex, actualInfo)
+					}
+					actualInfo.RootCall = nil
+					if *actualInfo != *expectInfo {
+						return fmt.Errorf("#%d: expected %#v, actual %#v", currentIndex, expectInfo, actualInfo)
+					}
+				}
+				if infoIndex != len(tt.expectInfos) {
+					return fmt.Errorf("wanted %d call expressions, got %d", len(tt.expectInfos), infoIndex)
+				}
+				return nil
+			})
 		})
 	}
 }
 
 func TestGomegaDotHandler_ReplaceFunction(t *testing.T) {
-	h := dotHandler{}
+	h := &Handler{name: "" /* no name, dot import! */}
 
 	expr := &ast.CallExpr{
 		Fun: ast.NewIdent("one"),
@@ -339,7 +261,7 @@ func TestGomegaDotHandler_ReplaceFunction(t *testing.T) {
 }
 
 func TestGomegaNameHandler_ReplaceFunction(t *testing.T) {
-	h := &nameHandler{name: "gomega"}
+	h := &Handler{name: "gomega"}
 
 	expr := &ast.CallExpr{
 		Fun: &ast.SelectorExpr{
--- testdata/src/a/noassersion/ginkgo.go
+++ testdata/src/a/noassersion/ginkgo.go
@@ -3,6 +3,8 @@ package noassersion
 import (
 	"time"
 
+	"a/wrappers"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -16,5 +18,18 @@ var _ = Describe("", func() {
 		EventuallyWithOffset(1, func(g Gomega) { g.Expect(true) }).WithTimeout(2 * time.Second) // want `ginkgo-linter: "EventuallyWithOffset": missing assertion method. Expected "Should\(\)" or "ShouldNot\(\)"` `ginkgo-linter: "Expect": missing assertion method. Expected "To\(\)", "ToNot\(\)" or "NotTo\(\)"`
 		ConsistentlyWithOffset(2, func() bool { return true })                                  // want `ginkgo-linter: "ConsistentlyWithOffset": missing assertion method\. Expected "Should\(\)" or "ShouldNot\(\)"`
 		Ω("omega")                                                                              // want `ginkgo-linter: "Ω": missing assertion method\. Expected "Should\(\)", "To\(\)", "ShouldNot\(\)", "ToNot\(\)" or "NotTo\(\)"`
+		wrappers.MyExpect("aaa")                                                                // want `ginkgo-linter: "MyExpect": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+		wrappers.MyEventually("aaa")                                                            // want `ginkgo-linter: "MyEventually": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+		wrappers.MyConsistently("aaa")                                                          // want `ginkgo-linter: "MyConsistently": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+		wrappers.NewAssertion("aaa")                                                            // want `ginkgo-linter: "NewAssertion": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+		wrappers.NewAsyncAssertion("aaa")                                                       // want `ginkgo-linter: "NewAsyncAssertion": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+
+		func() Assertion { // want `ginkgo-linter: "": missing assertion method\. Expected one of \"To/NotTo/ToNot\" \(for Expect assertions\) or \"Should/ShouldNot\" \(for Eventually/Consistently assertions\)`
+			return Expect("aaa")
+		}()
+
+		Eventually(func() error { // want `ginkgo-linter: "Eventually": missing assertion method\. Expected "Should\(\)" or "ShouldNot\(\)"`
+			return nil
+		}).WithTimeout(500 * time.Millisecond).WithPolling(10 * time.Millisecond)
 	})
 })
